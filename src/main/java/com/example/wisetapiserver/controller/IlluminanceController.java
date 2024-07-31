01cbc2fc91a1edd659c62cb7c018d415f0096691
youbbin <youbbqlsl@gmail.com>
2024-07-31 10:35:46 +0900

    feat: 태양 방위각, 고도각 계산

--- src/main/java/com/example/wisetapiserver/controller/IlluminanceController.java
+++ src/main/java/com/example/wisetapiserver/controller/IlluminanceController.java
@@ -1,12 +1,16 @@
 package com.example.wisetapiserver.controller;
 
 import com.example.wisetapiserver.domain.Illuminance;
-import com.example.wisetapiserver.service.IlluminanceService;
+import com.example.wisetapiserver.service.illuminance.IlluminanceService;
+import com.example.wisetapiserver.service.sunposition.SunPosition;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 
 @RestController
@@ -21,7 +25,16 @@ public class IlluminanceController {
         return "test";
     }
 
+    @GetMapping("/sun-position")
+    public double[] getSunPosition(@RequestParam String dateTime,
+                                   @RequestParam double lat,
+                                   @RequestParam double lng) {
+        DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE_TIME;
+        LocalDateTime localDateTime = LocalDateTime.parse(dateTime, formatter);
+        return SunPosition.calculateAzEl(localDateTime, lat, lng);
+    }
 
+    @GetMapping("/test")
     public Optional<Illuminance> getLatestData() {
         return illuminanceService.getLatestData();
     }
